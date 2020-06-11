5e21ff1e53f66e62389b043c379d102d74140042
Blaise Pabon <blaise.pabon@vgs.io>
2020-06-10 22:08:49 -0700

    initial commit

---
+++ project/app/config.py
@@ -0,0 +1,18 @@
+
+import logging
+import os
+
+from pydantic import BaseSettings
+
+
+log = logging.getLogger(__name__)
+
+
+class Settings(BaseSettings):
+    environment: str = os.getenv("ENVIRONMENT", "dev")
+    testing: bool = os.getenv("TESTING", 0)
+
+
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
