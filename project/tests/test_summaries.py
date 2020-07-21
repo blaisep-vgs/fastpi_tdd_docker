e235ae5ceb848ed13c131620d88d2e1c9bea6a23
Blaise Pabon <blaise.pabon@vgs.io>
2020-07-20 17:27:58 -0700

    fix flake8 issues

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -1,6 +1,5 @@
 # project/tests/test_summaries.py
 import json
-
 import pytest
 
 
@@ -10,20 +9,6 @@ def test_create_summary(test_app_with_db):
     )
     assert response.status_code == 201
     assert response.json()["url"] == "https://foo.bar"
-
-
-def test_create_summaries_invalid_json(test_app):
-    response = test_app.post("/summaries/", data=json.dumps({}))
-    assert response.status_code == 422
-    assert response.json() == {
-        "detail": [
-            {
-                "loc": ["body", "payload", "url"],
-                "msg": "field required",
-                "type": "value_error.missing",
-            }
-        ]
-    }
 
 
 def test_read_summary(test_app_with_db):
@@ -38,12 +23,6 @@ def test_read_summary(test_app_with_db):
     assert response_dict["url"] == "https://foo.bar"
     assert response_dict["summary"]
     assert response_dict["created_at"]
-
-
-def test_read_summary_incorrect_id(test_app_with_db):
-    response = test_app_with_db.get("/summaries/999/")
-    assert response.status_code == 404
-    assert response.json()["detail"] == "Summary not found"
 
 
 def test_read_all_summaries(test_app_with_db):
@@ -68,12 +47,6 @@ def test_remove_summary(test_app_with_db):
     assert response.json() == {"id": summary_id, "url": "https://foo.bar"}
 
 
-def test_remove_summary_incorrect_id(test_app_with_db):
-    response = test_app_with_db.delete("/summaries/999/")
-    assert response.status_code == 404
-    assert response.json()["detail"] == "Summary not found"
-
-
 def test_update_summary(test_app_with_db):
     response = test_app_with_db.post(
         "/summaries/", data=json.dumps({"url": "https://foo.bar"})
@@ -93,7 +66,7 @@ def test_update_summary(test_app_with_db):
     assert response_dict["created_at"]
 
 
-def test_create_summaries_invalid_json(test_app):
+def test_create_summaries_invalid_json(test_app_with_db):
     response = test_app.post("/summaries/", data=json.dumps({}))
     assert response.status_code == 422
     assert response.json() == {
@@ -214,7 +187,7 @@ def test_update_summary_invalid(
 
 def test_update_summary_invalid_url(test_app):
     response = test_app.put(
-        f"/summaries/1/",
+        f'{"/summaries/1/"}',
         data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
     )
     assert response.status_code == 422
